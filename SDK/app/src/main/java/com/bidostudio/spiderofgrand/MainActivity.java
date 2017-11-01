da3baebbf0c7d95ef4565aa1bfbdf007626dc618
Hoang Nguyen <hoang.nguyen2@niteco.se>
2017-11-01 14:33:02 +0700

    update grandle

--- SDK/app/src/main/java/com/bidostudio/spiderofgrand/MainActivity.java
+++ SDK/app/src/main/java/com/bidostudio/spiderofgrand/MainActivity.java
@@ -1,70 +1,51 @@
 package com.bidostudio.spiderofgrand;
 
-import android.os.StrictMode;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
 import com.appodeal.ads.Appodeal;
 import com.appodeal.ads.BannerCallbacks;
-import com.appodeal.ads.InterstitialCallbacks;
-
-import hoangnv.sdk.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final String APP_KEY_APPODEAL = "6536df740ef85e163641298f66b1ae5c067ff6fc49cda055";
+    private String APP_KEY_APPODEAL = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        APP_KEY_APPODEAL = getResources().getString(R.string.appodeal_key);
+
         Appodeal.disableLocationPermissionCheck();
         Appodeal.initialize(this, APP_KEY_APPODEAL, Appodeal.BANNER | Appodeal.INTERSTITIAL | Appodeal.REWARDED_VIDEO );
+        if (BuildConfig.DEBUG){
+            Appodeal.setTesting(false);
+        }
+        Appodeal.disableNetwork(this, "admob", Appodeal.INTERSTITIAL | Appodeal.REWARDED_VIDEO | Appodeal.BANNER );
+        Appodeal.disableNetwork(this, "facebook", Appodeal.INTERSTITIAL | Appodeal.REWARDED_VIDEO | Appodeal.BANNER);
 
-//        Appodeal.setBannerCallbacks(new BannerCallbacks() {
-//            @Override
-//            public void onBannerLoaded(int height, boolean isPrecache) {
-//                Log.d("Appodeal", "onBannerLoaded");
-//            }
-//            @Override
-//            public void onBannerFailedToLoad() {
-//                Log.d("Appodeal", "onBannerFailedToLoad");
-//            }
-//            @Override
-//            public void onBannerShown() {
-//                Log.d("Appodeal", "onBannerShown");
-//            }
-//            @Override
-//            public void onBannerClicked() {
-//                Log.d("Appodeal", "onBannerClicked");
-//            }
-//        });
+        Appodeal.setBannerCallbacks(new BannerCallbacks() {
+            @Override
+            public void onBannerLoaded(int height, boolean isPrecache) {
+                Log.d("Appodeal", "onBannerLoaded");
+            }
+            @Override
+            public void onBannerFailedToLoad() {
+                Log.d("Appodeal", "onBannerFailedToLoad");
+            }
+            @Override
+            public void onBannerShown() {
+                Log.d("Appodeal", "onBannerShown");
+            }
+            @Override
+            public void onBannerClicked() {
+                Log.d("Appodeal", "onBannerClicked");
+            }
+        });
 
-//        Appodeal.setInterstitialCallbacks(new InterstitialCallbacks() {
-//            @Override
-//            public void onInterstitialLoaded(boolean isPrecache) {
-//                Log.d("Appodeal", "onInterstitialLoaded");
-//            }
-//            @Override
-//            public void onInterstitialFailedToLoad() {
-//                Log.d("Appodeal", "onInterstitialFailedToLoad");
-//            }
-//            @Override
-//            public void onInterstitialShown() {
-//                Log.d("Appodeal", "onInterstitialShown");
-//            }
-//            @Override
-//            public void onInterstitialClicked() {
-//                Log.d("Appodeal", "onInterstitialClicked");
-//            }
-//            @Override
-//            public void onInterstitialClosed() {
-//                Log.d("Appodeal", "onInterstitialClosed");
-//            }
-//        });
 
         Appodeal.show(this, Appodeal.BANNER_BOTTOM);
 
